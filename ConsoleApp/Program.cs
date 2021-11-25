1465b0e4f30ce9934ce5e96544455e03477d6c0b
seva615 <1230123zz@gmail.com>
2021-11-26 01:01:37 +0300

    ForReview

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using TestGenerator;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new NUnitTestGenerator(10, 10, 10);
+            generator.GenerateTestsAsync(@"C:\Users\yug77_bd0zewu\Documents\GitHub\Spp-4\ConsoleApp\input",
+                @"C:\Users\yug77_bd0zewu\Documents\GitHub\Spp-4\ConsoleApp\output").Wait();
+        }
+    }
+}
